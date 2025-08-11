ec01c89bd1bda66b04db01d0bd23d1758570c7fb
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2025-08-11 10:11:09 +0100

    chore(cicd): Fix release github action (#9)
    * chore(cd): Fix semantic-release github action
    
    * chore(cd): Fix release branch for dev tag
    
    * chore(cd): don't run release action automatically on main

--- packages/capacitor-plugin/release.config.cjs
+++ packages/capacitor-plugin/release.config.cjs
@@ -2,13 +2,18 @@ module.exports = {
   branches: [
     { name: 'main', channel: 'latest' },
     { name: 'next', channel: 'next', prerelease: true },
-    { name: 'dev', channel: 'dev', prerelease: true },
+    { name: 'development', channel: 'dev', prerelease: true },
   ],
   repositoryUrl: 'https://github.com/ionic-team/capacitor-file-viewer.git',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "../../CHANGELOG.md"
+      }
+    ],
     '@semantic-release/npm',
     [
       '@semantic-release/github',
@@ -18,16 +23,16 @@ module.exports = {
         releasedLabels: false,
         addReleases: 'bottom',
         releaseNotes: {
-          changelogFile: 'CHANGELOG.md',
-        },
-      },
+          changelogFile: '../../CHANGELOG.md'
+        }
+      }
     ],
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
-  ],
+        assets: ['../../CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ]
 };
