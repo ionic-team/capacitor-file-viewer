47720e873525a1a12e10c743ac378ccd3f852d4d
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-09-08 12:59:23 +0100

    feat: Capacitor 8 support
    BREAKING CHANGE: Capacitor major version update requires major version update on the plugin.

--- packages/capacitor-plugin/Package.swift
+++ packages/capacitor-plugin/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorFileViewer",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorFileViewer",
