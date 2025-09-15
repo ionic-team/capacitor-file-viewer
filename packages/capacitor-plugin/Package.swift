4f8f2db0a37df34aacd05461d3f19a141235e58a
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-09-15 10:08:22 +0100

    fix(ios): Simplify SPM usage for native library

--- packages/capacitor-plugin/Package.swift
+++ packages/capacitor-plugin/Package.swift
@@ -10,20 +10,16 @@ let package = Package(
             targets: ["FileViewerPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/ionic-team/ion-ios-fileviewer.git", from: "1.0.1")
     ],
     targets: [
-        .binaryTarget(
-            name: "IONFileViewerLib",
-            url: "https://github.com/ionic-team/ion-ios-fileviewer/releases/download/1.0.1/IONFileViewerLib.zip",
-            checksum: "2dcd0291e664ffbb5636119f147eda266e0d2284aa53a9c99925d0e442a0e92d" // sha-256
-        ),
         .target(
             name: "FileViewerPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                "IONFileViewerLib"
+                .product(name: "IONFileViewerLib", package: "ion-ios-fileviewer")
             ],
             path: "ios/Sources/FileViewerPlugin"),
         .testTarget(
