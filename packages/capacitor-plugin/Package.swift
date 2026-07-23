2f6e26b2ea26fed4937b2803c0ad7d97b7e26f08
jcesarmobile <jcesarmobile@gmail.com>
2026-07-23 18:06:49 +0200

    chore!: Align plugin baselines with core (#34)

--- packages/capacitor-plugin/Package.swift
+++ packages/capacitor-plugin/Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorFileViewer",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapacitorFileViewer",
             targets: ["FileViewerPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5"),
         .package(url: "https://github.com/ionic-team/ion-ios-fileviewer.git", from: "1.0.3")
     ],
     targets: [
@@ -18,7 +18,6 @@ let package = Package(
             name: "FileViewerPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "IONFileViewerLib", package: "ion-ios-fileviewer")
             ],
             path: "ios/Sources/FileViewerPlugin"),
